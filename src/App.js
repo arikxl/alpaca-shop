6d5d348f6a57f675c48312a8a0179c82ac1f0175
Arik Alexandrov <arikxl@gmail.com>
2021-10-01 09:44:01 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
 import './styles/App.css';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
@@ -5,13 +7,38 @@ import Login from './pages/Login';
 import Product from './pages/Product';
 import Products from './pages/Products';
 import Register from './pages/Register';
+import ProductList from './components/ProductList';
+import ProductItem from './components/ProductItem';
 
-function App() {
+
+const App = () => {
+
+  const user = true;
+
   return (
-    <div className="App">
-        <Home />
-        {/* <Cart /> */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path ="/">
+          <Home />
+        </Route>
+        <Route path ="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path ="/product/:id">
+          <Product />
+        </Route>
+        <Route path ="/cart">
+          <Cart />
+        </Route>
+        <Route path ="/login">
+          {user ? <Redirect to="/" /> : <Login />}
+        </Route>
+        <Route path ="/register">
+        {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export  const sliderItems = [
     {
         id: 1,
-        img: "https://i.ibb.co/XsdmR2c/1.png",
+        img: "https://i.ibb.co/cXFnLLV/3.png",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "#fbf0f4"
@@ -15,7 +15,7 @@ export  const sliderItems = [
     },  
     {
         id: 3,
-        img: "https://i.ibb.co/cXFnLLV/3.png",
+        img: "https://i.ibb.co/XsdmR2c/1.png",
         title: "SPRING COLLECTION",
         desc: "IT'S TIME TO REVIVE YOUR STYLE. LOOK STUNNING IN ANY SEASON. EVERYWHERE.",
         bg: "#fcf1ed"
